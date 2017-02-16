623f9c8c96e04be3cc1536b9c1431c43572320b6
Evgeniy Pozdeev <evgeniypozdeev@MacBook-Air-Evgeniy.local>
2017-02-16 21:07:30 +0300

    Add base project

---
+++ iprog1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  iprog1
+//
+//  Created by Evgeniy Pozdeev on 16.02.17.
+//  Copyright © 2017 Evgeniy Pozdeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myButton: UIButton!
+
+    @IBOutlet weak var myLabel: UILabel!
+
+    @IBOutlet weak var myText: UITextField!
+    
+    @IBAction func myButtonTaped(_ sender: AnyObject) {
+        
+          if let text = myText.text {
+            myLabel.text = "Hello, " + text
+            }
+        
+        //myLabel.text = ("Hello! World")
+    
+}
+
+}
+
